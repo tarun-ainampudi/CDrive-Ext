b5c6da02ccde4ff7f8b6fac6ae85222bfafa61a3
tarun-ainampudi <tarunainampudi@gmail.com>
2026-06-04 10:57:11 +0530

    feat: add auto login by saving credentials into extension local storage

---
+++ background.js
@@ -0,0 +1,46 @@
+function checkForTabsAndReturnIds(tabs) {
+    const filteredTabs = tabs.filter(tab => {
+        return tab.url !== undefined && (tab.url.includes("cdc.vit.ac.in") || tab.url.includes("examly.io"));
+    });
+    if(filteredTabs.length === 1) {
+        return [filteredTabs[0].id];
+    }
+    else if (filteredTabs.length > 1) {
+        const secondFilter = filteredTabs.filter((tab) => {
+            return tab.url.includes("/dashboard")
+                || tab.url.includes("/login")
+                || tab.url.includes("/mycourses")
+        })
+        if (secondFilter.length !== 0)
+            return secondFilter.map(tab => tab.id);
+        return filteredTabs.map(tab => tab.id);
+    }
+    return [];
+}
+async function getTabIdsForLoginHandler() {
+    const tabs = await chrome.tabs
+        .query({
+            active: true,
+        });
+    return checkForTabsAndReturnIds(tabs);
+}
+async function injectLoginHandler(tabIndex) {
+    const tabIds = await getTabIdsForLoginHandler();
+    console.log("[Inject Login Handler] Tab Ids: ", tabIds);
+    for (const id of tabIds) {
+        chrome.scripting.executeScript({
+            target: { tabId: id, allFrames: true },
+            files: ["scripts/login_handler.js"],
+        }).then(() => console.log("Login Handler Injection Initiated"));
+    }
+}
+
+chrome.webRequest.onCompleted.addListener((details) => {
+    console.log("[Completed Request]:", details);
+    if (details.url.includes("/logout") ||
+        (details.url.includes("/login") && details.method === 'GET')
+    ) {
+        injectLoginHandler(details.tabId);
+    }
+
+}, { urls: ["*://api.examly.io/api/*"] });
---
+++ scripts/login_handler.js
@@ -0,0 +1,97 @@
+function getLoginCredsToSave() {
+    const emailInput = document.querySelector("#email");
+    const passwordInput = document.querySelector("#password");
+    if (window.location.href.includes("/pwd")
+        && emailInput !== null && passwordInput !== null) {
+        return {
+            'email': emailInput.value.trim(),
+            'password': passwordInput.value
+        };
+    }
+    return {};
+}
+
+function attachObserverToSubmitBtn() {
+    const formDiv = document.querySelector(".formPos");
+    const submitBtn = formDiv?.querySelector('button');
+    if (submitBtn) {
+        console.log("[Login Handler] Attaching Observer to Submit Button");
+        submitBtn.onclick = (event) => {
+            const creds = getLoginCredsToSave();
+            if (Object.keys(creds).length === 2) {
+                console.log(`[Login Handler] Save Creds: ${JSON.stringify(creds)}`);
+                return storeCredentials(creds);
+            }
+            console.log(`[Login Handler] Invalid Creds Obj: ${JSON.stringify(creds)}`);
+        }
+        return true;
+    }
+    console.log("[Login Handler] Submit Button Not Found");
+    return false;
+}
+
+async function storeCredentials(creds) {
+    await chrome.storage.local.set({ 'loginCredentials': creds })
+    console.log("[Login Handler] Credentials Stored Successfully");
+}
+
+async function getStoredCredentials() {
+    const creds = await chrome.storage.local.get('loginCredentials');
+    console.log(`[Login Handler] Get Creds Obj: ${JSON.stringify(creds)}`);
+    if (Object.keys(creds).includes('loginCredentials')
+        && creds.loginCredentials.email !== undefined
+        && creds.loginCredentials.password !== undefined) {
+        return creds.loginCredentials;
+    }
+    return null;
+}
+
+function fillCredentials(creds) {
+    let status = false;
+    const formDiv = document.querySelector(".formPos");
+    const submitBtn = formDiv?.querySelector('button');
+    const emailInput = document.querySelector("#email");
+    const passwordInput = document.querySelector("#password");
+    if (emailInput !== null && emailInput.value === '') {
+        emailInput.value = creds.email;
+        emailInput.dispatchEvent(new Event("input", { bubbles: true }));
+    }
+    if (passwordInput !== null && passwordInput.value === '') {
+        passwordInput.value = creds.password;
+        passwordInput.dispatchEvent(new Event("input", { bubbles: true }));
+        status = true;
+    }
+    if (submitBtn) {
+        submitBtn.click();
+    }
+    return status;
+}
+
+function addListenerToAutoFill(creds) {
+    if (creds) {
+        fillCredentials(creds);
+    }
+    document.addEventListener("load", () => {
+        if (creds) fillCredentials(creds)
+        else attachObserverToSubmitBtn();
+    }, true);
+}
+
+async function loginHandlerInit() {
+    console.log("[Login Handler] Script Injected");
+    const creds = await getStoredCredentials();
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", (event) => {
+            addListenerToAutoFill(creds);
+        });
+    } else {
+        addListenerToAutoFill(creds);
+    }
+}
+
+(() => {
+    if (!window.location.href.includes("/pwd")
+        && !window.location.href.includes("/login"))
+        return;
+    loginHandlerInit()
+})();
