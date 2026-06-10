1e49a629c1d9812604ad944e5cfe4396b3b3a722
tarun-ainampudi <tarunainampudi@gmail.com>
2026-06-10 11:31:07 +0530

    feat: add methods to support multiple language handling

--- content_scripts/modules/cod/test_page.js
+++ content_scripts/modules/cod/test_page.js
@@ -1,7 +1,64 @@
+function checkForDuplicationOfQs(solvedKey) {
+    const seen = new Set();
+    for (const { question } of solvedKey) {
+        if (seen.has(question)) {
+            return true;
+        }
+        seen.add(question);
+    }
+    return false;
+}
+
+function mergeSameQuestionsIntoOne(solvedKey) {
+    const tempDic = {};
+    for (const { question, language, code } of solvedKey) {
+        tempDic[question] ??= { question };
+        tempDic[question][language] = code;
+    }
+    const mergedSolvedKey = Object.values(tempDic);
+    console.log(`[COD Test] Merged solvedKey: ${JSON.stringify(mergedSolvedKey)}`);
+    return mergedSolvedKey;
+}
+
 function fillCodeInInput(code, inputElement) {
     inputElement.value = code;
     inputElement.dispatchEvent(new Event('input', { bubbles: true }));
     return 'code';
+}
+
+function selectLanguageOfCode(lang) {
+    const dropDownDivs = document.querySelectorAll('.mydropdown');
+    if (dropDownDivs.length === 0) {
+        console.log(`[COD Test] Cannot find dropDown to Select Language`);
+        return '';
+    } else if (dropDownDivs.length === 1) {
+        dropDownDivs[0].click();
+        const langOptions = [...document.querySelectorAll('.each-option')];
+        const langNames = langOptions.map((div) => {
+            return div.innerText.replace(/[^A-Za-z+]/g, '').toLowerCase();
+        });
+        const isSelected = langOptions.some((option, index) => {
+            if (lang === langNames[index]) {
+                option.click();
+                return true;
+            }
+            return false;
+        });
+        let javaIndex;
+        if (!isSelected && (javaIndex = langNames.indexOf('java')) !== -1) {
+            console.log(`[COD Test] Selected default java language`);
+            langOptions[javaIndex].click();
+            return 'java';
+        } else if (!isSelected) {
+            console.log(`[COD Test] Cannot find languages to select in dropDown`);
+            return '';
+        } else {
+            return lang;
+        }
+    } else {
+        console.log(`[COD Test] Multiple dropDowns found!`);
+        return '';
+    }
 }
 
 function answerBasedOnTheLength(solvedKey, inputElement) {
@@ -42,7 +99,7 @@ function answerCodIfExistInKey(question, solvedKey, inputElement) {
             answerIndex = index;
             acc = acc + 1;
         }
-        if (checkMatchBasedOnIncludes(parsedQuestion,solvedQ)) {
+        if (checkMatchBasedOnIncludes(parsedQuestion, solvedQ)) {
             nOfSQsIncludesQ.push(index);
         }
         return acc;
